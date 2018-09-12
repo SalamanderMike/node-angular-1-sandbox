bb6e8e5dc935a6a6bb518cf34751c63be76bd2cd
Michael McClure <MichaelJohnMcClure@me.com>
2018-09-11 17:41:36 -0700

    Implemented Bubble Pop Functionality

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -14,6 +14,8 @@ export default function AppController ($scope, $animateCss, $timeout, $q) {
 		gameState = state;
 		secureClosureCount = 0;
 
+
+// REFACTOR $scope.stateOfGame LABELING TO REDUCE IF TO TWO CHOICES
 		if (gameState === "new") {
 			$scope.stateOfGame = "Pause";
 			moveBubbles();
@@ -26,68 +28,60 @@ export default function AppController ($scope, $animateCss, $timeout, $q) {
 		}
 	};
 
-	$scope.collectScore = function(score) {
-		$scope.score += score;
+	$scope.popBubble = function(element) {
+		$scope.score += parseInt(element.target.innerHTML,10);
+		let targetBubble = document.getElementById(element.target.id);
+		let pos = Math.floor(Math.random() * (80 - 1)) + 1;
+		let newSize = parseInt(window.getComputedStyle(targetBubble).width,10) *0.99;
+		let newValue = parseInt(targetBubble.innerHTML,10) +10;
+		let newDuration = parseFloat(window.getComputedStyle(targetBubble).animationDuration) - 0.2;
+
+		targetBubble.classList.remove("bubble");
+		void targetBubble.offsetWidth;
+
+		targetBubble.classList.add("bubble");
+		targetBubble.style.animationDuration = newDuration +'s';
+		targetBubble.style.animationDelay = 2 +'s';
+		targetBubble.style.width = newSize +'px';
+		targetBubble.style.height = newSize +'px';
+		targetBubble.style.left = pos +'%';
+		targetBubble.innerHTML = newValue;
 	}
 
 	function getAnimationState() {
 		animations = document.querySelectorAll('.bubble');
-		// console.log(".bubble Children:",animations);
 		return animations;
 	}
-
-	 $scope.getAnimationState = function() {
-		animations = document.querySelectorAll('.bubble');
-		console.log(".bubble Children",animations);
-	}
-
-
 
 	function moveBubbles() {
 		$q.when(getAnimationState()).then(function(animations) {
 			for (let i = 0; i < animations.length; i++) {
 				var initialSpeed = Math.floor(Math.random() * (7000 - 4500)) + 4500;
-				animations[i].style.webkitAnimationPlayState = 'running';
-				animations[i].animate([
-					{ transform: 'translateY(10vh)' }, 
-					{ transform: 'translateY(100vh)' }
-					], { 
-					duration: initialSpeed,
-					iterations: Infinity,
-					delay: i * 1000
-				});
+
+				animations[i].style.animationDuration = initialSpeed +'ms';
+				animations[i].style.animationDelay = i +'s';
+				animations[i].style.webkitAnimationPlayState = 'running';			
 			};
 		})
 	}
 
 	function pauseLoop() {
-		// console.log("In LOOP");
-		$timeout.cancel(bubblePerSecond);
-		if (gameState === "pause") {
-			// console.log("In PAUSE");
-			secureClosureCount++;
-			$q.when(getAnimationState()).then(function(animations) {
-				// console.log("animations.length",animations.length);
-				for (let i = 0; i < animations.length; i++) {
+		$q.when(getAnimationState()).then(function(animations) {
+			for (let i = 0; i < animations.length; i++) {
+				if(gameState === "pause") { 
 					animations[i].style.webkitAnimationPlayState = 'paused';
-					// console.log("webkitAnimationPlayState",animations[i].style.webkitAnimationPlayState);
-				};
-			}, (function() {
-				if (secureClosureCount < 10) {
-					bubblePerSecond = $timeout(pauseLoop, 100)
 				} else {
-					$timeout.cancel(bubblePerSecond);
-				}
-			})());
-		} else {
-			// console.log("In RESUME");
-			$q.when(getAnimationState()).then(function(animations) {
-				for (let i = 0; i < animations.length; i++) {
-					animations[i].style.webkitAnimationPlayState = 'running';
-				};
-			}, moveBubbles());
-		}
+					animations[i].style.webkitAnimationPlayState = 'running';  
+				}  
+			};
+		})
 	}
+
+
+
+
+
+		
 
 
 
--- app/directives/bubble.factory.js
+++ app/directives/bubble.factory.js
@@ -4,20 +4,21 @@ export default function bubbleFactory() {
 			restrict: 'EA',
 			template: function(element, attrs){
 				let htmlText;
-				for (let i = 0; i < 3; i++) {
+				for (let i = 0; i < 29; i++) {
+					let id = i.toString();
 					let score = Math.floor(Math.random() * (99 - 1)) + 1;
 					let pos = Math.floor(Math.random() * (80 - 1)) + 1;
 					let size = Math.floor(Math.random() * (85 - 20)) + 20;
 					htmlText = '<div class="bubble" ng-model="bubble" '
+											+'id="'+id+'"'
 											+'style="left:'+pos+'%;'
 											+'width:'+size+'px;'
 											+'height:'+size+'px;'
 											+'" '
-											+'ng-click="collectScore('+score+')">'+score
+											+'data-ng-click="popBubble($event)">'+score
 											+'</div>' 
 											+ htmlText;
 				}
-					console.log("Bubbles Built");
 					return htmlText;						
 			}
 	};
