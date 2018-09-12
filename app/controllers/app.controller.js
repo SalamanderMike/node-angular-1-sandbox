396c561fe74a9813beecbcfaeb0e6d397fd26235
Michael McClure <MichaelJohnMcClure@me.com>
2018-09-12 13:21:04 -0700

    Midpoint Refactor

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -1,82 +1,94 @@
 export default function AppController ($scope, $animateCss, $timeout, $q) {
 	'ngInject';
-	var app = this;
-	var gameState;
-	var animations = [];
-	var bubblePerSecond = null;
-	var secureClosureCount = 0;
-	$scope.stateOfGame = "Start";
+	const app = this;
+	$scope.gameState = "Start";
 	$scope.score = 0;
 	$scope.speed = 10;
 
-	$scope.toggleAnimation = function(state) {
-		var style;
-		gameState = state;
-		secureClosureCount = 0;
-
-
-// REFACTOR $scope.stateOfGame LABELING TO REDUCE IF TO TWO CHOICES
-		if (gameState === "new") {
-			$scope.stateOfGame = "Pause";
-			moveBubbles();
-		} else if (gameState === "pause") {
-			$scope.stateOfGame = "Resume";
-			pauseLoop();
-		} else {
-			$scope.stateOfGame = "Pause";
-			pauseLoop();
-		}
+	$scope.gameStateChange = function(gameState) {
+		 if (gameState === 'Start') {
+		 	$scope.gameState = "Pause";
+		 	toggleAnimation();
+		 } else {
+		 	$scope.gameState = gameState;
+		 	toggleAnimation(gameState);
+		 }
 	};
 
 	$scope.popBubble = function(element) {
+		var targetBubble = document.getElementById(element.target.id);
 		$scope.score += parseInt(element.target.innerHTML,10);
-		let targetBubble = document.getElementById(element.target.id);
-		let pos = Math.floor(Math.random() * (80 - 1)) + 1;
-		let newSize = parseInt(window.getComputedStyle(targetBubble).width,10) *0.99;
-		let newValue = parseInt(targetBubble.innerHTML,10) +10;
-		let newDuration = parseFloat(window.getComputedStyle(targetBubble).animationDuration) - 0.2;
-
 		targetBubble.classList.remove("bubble");
 		void targetBubble.offsetWidth;
-
 		targetBubble.classList.add("bubble");
-		targetBubble.style.animationDuration = newDuration +'s';
-		targetBubble.style.animationDelay = 2 +'s';
-		targetBubble.style.width = newSize +'px';
-		targetBubble.style.height = newSize +'px';
-		targetBubble.style.left = pos +'%';
-		targetBubble.innerHTML = newValue;
+
+		$q.when(newParams(targetBubble)).then(function(param) {
+			targetBubble.style.animationDuration = param.fallSpeed +'s';
+			targetBubble.style.height = param.size +'px';
+			targetBubble.style.width = param.size +'px';
+			targetBubble.style.lineHeight = (param.size) +'px';
+			targetBubble.innerHTML = param.scorePoints;
+			targetBubble.style.animationDelay = 2 +'s';
+			targetBubble.style.left = param.posX +'%';
+		})
 	}
+
+	function toggleAnimation(gameState) {
+		switch (gameState) {
+			case 'Pause':
+				$q.when(getAnimationState()).then(function(animations) {
+					for (let i = 0; i < animations.length; i++) {
+						animations[i].style.webkitAnimationPlayState = 'running';
+					};
+				})
+				break;
+			case 'Resume':
+				$q.when(getAnimationState()).then(function(animations) {
+					for (let i = 0; i < animations.length; i++) {
+						animations[i].style.webkitAnimationPlayState = 'paused';
+					};
+				})
+				break;
+			default:
+				// START
+				$q.when(getAnimationState()).then(function(animations) {
+					for (let i = 0; i < animations.length; i++) {
+						animations[i].style.animationDuration = newParams().fallSpeed +'s';
+						animations[i].style.webkitAnimationPlayState = 'running';			
+						animations[i].style.animationDelay = i +'s';
+					};
+				}
+			)
+		}
+	}
+
+	function newParams(targetBubble) {
+		if (targetBubble) {
+			return {
+				newFallSpeed: parseFloat(window.getComputedStyle(targetBubble).animationDuration) - 0.2,
+				size: parseInt(window.getComputedStyle(targetBubble).width,10) *0.99,
+				scorePoints: parseInt(targetBubble.innerHTML,10) +10,
+				posX: Math.floor(Math.random() * (80 - 1)) + 1
+			}
+		} else {
+			return {
+				fallSpeed: Math.random() * (7 - 4.5) + 4.5
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
 
 	function getAnimationState() {
-		animations = document.querySelectorAll('.bubble');
-		return animations;
+		return document.querySelectorAll('.bubble');
 	}
-
-	function moveBubbles() {
-		$q.when(getAnimationState()).then(function(animations) {
-			for (let i = 0; i < animations.length; i++) {
-				var initialSpeed = Math.floor(Math.random() * (7000 - 4500)) + 4500;
-
-				animations[i].style.animationDuration = initialSpeed +'ms';
-				animations[i].style.animationDelay = i +'s';
-				animations[i].style.webkitAnimationPlayState = 'running';			
-			};
-		})
-	}
-
-	function pauseLoop() {
-		$q.when(getAnimationState()).then(function(animations) {
-			for (let i = 0; i < animations.length; i++) {
-				if(gameState === "pause") { 
-					animations[i].style.webkitAnimationPlayState = 'paused';
-				} else {
-					animations[i].style.webkitAnimationPlayState = 'running';  
-				}  
-			};
-		})
-	}
-
 
 
 
--- app/directives/bubble.factory.js
+++ app/directives/bubble.factory.js
@@ -1,25 +1,26 @@
 export default function bubbleFactory() {
 	'ngInject';
 	return {
-			restrict: 'EA',
-			template: function(element, attrs){
-				let htmlText;
-				for (let i = 0; i < 29; i++) {
-					let id = i.toString();
-					let score = Math.floor(Math.random() * (99 - 1)) + 1;
-					let pos = Math.floor(Math.random() * (80 - 1)) + 1;
-					let size = Math.floor(Math.random() * (85 - 20)) + 20;
-					htmlText = '<div class="bubble" ng-model="bubble" '
-											+'id="'+id+'"'
-											+'style="left:'+pos+'%;'
-											+'width:'+size+'px;'
-											+'height:'+size+'px;'
-											+'" '
-											+'data-ng-click="popBubble($event)">'+score
-											+'</div>' 
-											+ htmlText;
-				}
-					return htmlText;						
+		restrict: 'EA',
+		template: function(element, attrs){
+			var htmlText;
+			for (let i = 0; i < 29; i++) {
+				let id = i.toString();
+				let score = Math.floor(Math.random() * (99 - 1)) + 1;
+				let pos = Math.floor(Math.random() * (80 - 1)) + 1;
+				let size = Math.floor(Math.random() * (85 - 20)) + 20;
+				htmlText = '<div class="bubble" ng-model="bubble" '
+								+'id="'+id+'"'
+								+'style="left:'+pos+'%;'
+								+'width:'+size+'px;'
+								+'height:'+size+'px;'
+								+'line-height:'+size+'px'
+								+'" '
+								+'data-ng-click="popBubble($event)">'+score
+								+'</div>' 
+								+ htmlText;
 			}
+			return htmlText;
+		}
 	};
 };
